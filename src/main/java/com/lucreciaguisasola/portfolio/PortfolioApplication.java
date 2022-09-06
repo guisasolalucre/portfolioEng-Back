15312e60503a9d813f839f89522d33b94bbcbc01
Lucrecia Guisasola <lucreciaguisasola@hotmail.com>
2022-09-06 17:48:28 +0800

    no cors

--- src/main/java/com/lucreciaguisasola/portfolio/PortfolioApplication.java
+++ src/main/java/com/lucreciaguisasola/portfolio/PortfolioApplication.java
@@ -19,7 +19,7 @@ public class PortfolioApplication {
                 @Override
                 public void addCorsMappings(CorsRegistry registry){
                     registry.addMapping("/*").
-                            allowedOrigins("https://lucreciaguisasola-portfolioen.web.app/portfolio").
+                            allowedOrigins("https://lucreciaguisasola-portfolioen.web.app/").
                             allowedHeaders("*").
                             allowedMethods("GET", "POST", "PUT", "DELETE");
                 }
